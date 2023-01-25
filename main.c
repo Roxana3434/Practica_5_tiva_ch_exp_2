3de898082a51dc1585b700c97f6b6bc15943ba66
Roxana3434 <112962134+Roxana3434@users.noreply.github.com>
2023-01-24 23:34:10 -0600

    Versión 2
    Dado a los problemas que se tuvieron para obtener el la frecuencia de 50 Hz , por el registro RCC en la tiva chica, decidí realizar el experimento con la tiva grande, configurando el PWM, ADC y main

--- main.c
+++ main.c
@@ -1,11 +1,5 @@
 #include "lib/include.h"
 
-uint16_t Result[3];
-uint16_t duty[3];
-
-int main(void)
-{
-   
 //Experimento 2
 /*Usando el modulo 0 de PWM con una frecuencia de reloj del sistema de 20,000,000 Hz
  * junto con el generador 0,1,2  habilitar alguno de los pwm's asociados y obtener un PWM
@@ -14,22 +8,28 @@ int main(void)
  *
  */
 
-    Configurar_PLL(_20MHZ);  //Confiuracion de velocidad de reloj
-    Configurar_UART0();
+
+
+int main(void)
+{
+    //Variables 
+    uint16_t Result[3];
+    uint16_t duty[3];
+
+    //Funciones para configuración 
+    Configurar_PLL(); 
+    //Configurar_UART0();
+    Configura_Reg_PWM0(8);
     Configura_Reg_ADC0();
-    Configura_Reg_PWM1(8);
-   
 
-    while (1)
+    while(1) 
     {
+        //Se llama a la función para leer y convertir valores del ADC
+        ADC0_InSeq2(Result,duty);
 
-
-
-       //ADC0_InSeq2(Result,duty); //llamada a la conversion por procesador
-       //PWM0->_0_CMPB = duty[0];
-       //PWM0->_1_CMPA = duty[1];
-       //PWM0->_2_CMPA = duty[2];
+        //modificar ciclo de trabajo 
+        PWM0->_0_CMPB = duty[0];
+        PWM0->_1_CMPA = duty[1];
+        PWM0->_2_CMPA = duty[2];
     }
-    
 }
-
